8145c7ff077a9127063fcde3af7280cf2088af21
sanskritikumari4444@gmail.com <sanskritikumari4444@gmail.com>
2025-03-08 16:38:58 +0530

    adding_first_code

---
+++ FirstClass.java
@@ -0,0 +1,192 @@
+import java.util.*;
+
+class FirstClass {
+    public static void main(String args[]) {
+        // System.out.println("Hello World");
+
+        Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int x = sc.nextInt();
+
+        // if (x % 2 == 0) {
+        // System.out.println("Even");
+        // } else {
+        // System.out.println("odd");
+        // }
+
+        // int a, b;
+        // a = sc.nextInt();
+        // b = sc.nextInt();
+        // if (a == b) {
+        // System.out.println("Equal");
+        // } else if (a > b) {
+        // System.out.println("a is greater");
+        // } else {
+        // System.out.println("a is lesser");
+        // }
+
+        // int button = sc.nextInt();
+
+        // switch (button) {
+        // case 1:
+        // System.out.println("Hello");
+        // break;
+        // case 2:
+        // System.out.println("Namaste");
+        // break;
+        // case 3:
+        // System.out.println("Namaste");
+        // break;
+        // case 4:
+        // default:
+        // System.out.println("Bonjour");
+        // }
+
+        // int counter = 0;
+        // while (counter < 3) {
+        // System.out.println("Hello World");
+        // counter = counter + 1;
+        // }
+
+        // for (int counter = 0; counter < 3; counter++) {
+        // System.out.println("Hello World");
+        // }
+
+        // No terminating condition - Infinite loop
+        // for (int counter = 0;; counter++) {
+        // System.out.println("Hello World");
+        // }
+
+        // for (int i = 0; i < 11; i++) {
+        // // System.out.println("Hello World");
+        // // System.out.println(i);
+        // System.out.print(i + " ");
+        // }
+
+        // Do while has a terminater
+        // int i = 0;
+        // do {
+        // System.out.print(i + " ");
+        // } while (i < 11);
+
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // for (int i = 1; i <= n; i++) {
+        // sum += i;
+        // }
+        // System.out.println(sum);
+
+        // int a = sc.nextInt();
+        // for (int i = 1; i <= 10; i++) {
+        // System.out.println(a + " * " + i + " = " + a * i);
+        // }
+
+        // int row = sc.nextInt();
+        // int col = sc.nextInt();
+        // for (int i = 1; i <= row; i++) {
+        // for (int j = 1; j <= col; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println("");
+        // }
+
+        // int row = sc.nextInt();
+        // int col = sc.nextInt();
+        // for (int i = 1; i <= row; i++) {
+        // for (int j = 1; j <= col; j++) {
+        // if (i == 1 || j == 1 || i == row || j == col) {
+        // System.out.print("* ");
+        // } else {
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println("");
+        // }
+
+        // int row = sc.nextInt();
+        // for (int i = 1; i <= row; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println("");
+        // }
+
+        // int row = sc.nextInt();
+        // for (int i = 1; i <= row; i++) {
+        // for (int j = 1; j <= row - i + 1; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println("");
+        // }
+        // for (int i = row; i >= 1; i--) {
+        // for (int j = i; j >= 1; j--) {
+        // System.out.print("* ");
+        // }
+        // System.out.println("");
+        // }
+
+        // int row = 4;
+        // for (int i = 1; i <= row; i++) {
+        // for (int j = 1; j <= row - i; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int k = 1; k <= i; k++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println("");
+        // }
+
+        // int row = 5;
+        // for (int i = 1; i <= row; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j + " ");
+        // }
+
+        // System.out.println("");
+        // }
+
+        // int row = 5;
+        // for (int i = row; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println("");
+        // }
+
+        // int count = 1;
+        // int row = 5;
+        // for (int i = 1; i <= row; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(count++ + " ");
+        // }
+        // System.out.println(" ");
+        // }
+
+        // int row = 5;
+        // for (int i = 1; i <= row; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // if ((i + j) % 2 == 0) {
+        // System.out.print("1 ");
+        // } else {
+        // System.out.print("0 ");
+        // }
+        // }
+        // System.out.println(" ");
+        // }
+
+        // int row = 5;
+        // for (int i = 1; i <= row; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(i + j - 1 + " ");
+        // }
+        // System.out.println(" ");
+        // }
+
+         
+
+        sc.close();
+
+    }
+}
